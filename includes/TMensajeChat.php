5877176b5d16c09b300cdcb3c94bd7fc83b6231b
Dani <53879220+GeoDNSO@users.noreply.github.com>
2020-03-07 11:52:47 +0100

    Transfers

---
+++ includes/TMensajeChat.php
@@ -0,0 +1,122 @@
+<?php
+
+class TMensajeChat{
+
+    private $idMensaje;
+    private $idChat;
+    private $idUsuario;
+    private $texto;
+    private $fecha;
+    
+
+    function __construct($idMensaje, $idChat, $idUsuario, $texto, $fecha){
+        $this->idMensaje = $idMensaje;
+        $this->idChat = $idChat;
+        $this->idUsuario = $idUsuario;
+        $this->texto = $texto;
+        $this->fecha = $fecha;
+    }
+
+
+    /**
+     * Get the value of idMensaje
+     */ 
+    public function getIdMensaje()
+    {
+        return $this->idMensaje;
+    }
+
+    /**
+     * Set the value of idMensaje
+     *
+     * @return  self
+     */ 
+    public function setIdMensaje($idMensaje)
+    {
+        $this->idMensaje = $idMensaje;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of idChat
+     */ 
+    public function getIdChat()
+    {
+        return $this->idChat;
+    }
+
+    /**
+     * Set the value of idChat
+     *
+     * @return  self
+     */ 
+    public function setIdChat($idChat)
+    {
+        $this->idChat = $idChat;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of idUsuario
+     */ 
+    public function getIdUsuario()
+    {
+        return $this->idUsuario;
+    }
+
+    /**
+     * Set the value of idUsuario
+     *
+     * @return  self
+     */ 
+    public function setIdUsuario($idUsuario)
+    {
+        $this->idUsuario = $idUsuario;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of texto
+     */ 
+    public function getTexto()
+    {
+        return $this->texto;
+    }
+
+    /**
+     * Set the value of texto
+     *
+     * @return  self
+     */ 
+    public function setTexto($texto)
+    {
+        $this->texto = $texto;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of fecha
+     */ 
+    public function getFecha()
+    {
+        return $this->fecha;
+    }
+
+    /**
+     * Set the value of fecha
+     *
+     * @return  self
+     */ 
+    public function setFecha($fecha)
+    {
+        $this->fecha = $fecha;
+
+        return $this;
+    }
+}
+
+?>
--- includes/TTema.php
+++ includes/TTema.php
@@ -1,3 +1,33 @@
 <?php
 
+class TTema{
+
+    private $tema;
+    
+    function __construct($tema){
+        $this->tema = $tema;
+    }
+
+
+    /**
+     * Get the value of tema
+     */ 
+    public function getTema()
+    {
+        return $this->tema;
+    }
+
+    /**
+     * Set the value of tema
+     *
+     * @return  self
+     */ 
+    public function setTema($tema)
+    {
+        $this->tema = $tema;
+
+        return $this;
+    }
+}
+
 ?>
