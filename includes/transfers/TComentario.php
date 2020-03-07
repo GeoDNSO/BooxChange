935c709f0ff0caca03c21a6b1680cf5851f704ce
MondlichtFox <49974534+MondlichtFox@users.noreply.github.com>
2020-03-07 12:07:12 +0100

    Add files via upload

--- includes/transfers/TComentario.php
+++ includes/transfers/TComentario.php
@@ -1,3 +1,120 @@
 <?php
 
+class TComentario{
+
+    private $idComentario;
+    private $idUsuario;
+    private $texto;
+    private $fecha;
+    private $idDiscusion;
+
+    function __construct($idComentario, $idUsuario, $texto, $fecha, $idDiscusion){
+        $this->idComentario = $idComentario;
+        $this->idUsuario = $idUsuario;
+        $this->texto = $texto;
+        $this->fecha = $fecha;
+        $this->idDiscusion = $idDiscusion;
+    }
+
+    /**
+     * Get the value of idComentario
+     */ 
+    public function getIdComentario()
+    {
+        return $this->idComentario;
+    }
+
+    /**
+     * Set the value of idComentario
+     *
+     * @return  self
+     */ 
+    public function setIdComentario($idComentario)
+    {
+        $this->idComentario = $idComentario;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of idUsuario
+     */ 
+    public function getIdUsuario()
+    {
+        return $this->idUsuario;
+    }
+
+    /**
+     * Set the value of idUsuario
+     *
+     * @return  self
+     */ 
+    public function setIdUsuario($idUsuario)
+    {
+        $this->idUsuario = $idUsuario;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of texto
+     */ 
+    public function getTexto()
+    {
+        return $this->texto;
+    }
+
+    /**
+     * Set the value of texto
+     *
+     * @return  self
+     */ 
+    public function setTexto($texto)
+    {
+        $this->texto = $texto;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of fecha
+     */ 
+    public function getFecha()
+    {
+        return $this->fecha;
+    }
+
+    /**
+     * Set the value of fecha
+     *
+     * @return  self
+     */ 
+    public function setFecha($fecha)
+    {
+        $this->fecha = $fecha;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of idDiscusion
+     */ 
+    public function getIdDiscusion()
+    {
+        return $this->idDiscusion;
+    }
+
+    /**
+     * Set the value of idDiscusion
+     *
+     * @return  self
+     */ 
+    public function setIdDiscusion($idDiscusion)
+    {
+        $this->idDiscusion = $idDiscusion;
+
+        return $this;
+    }
+}
+
 ?>
--- includes/transfers/TGenero.php
+++ includes/transfers/TGenero.php
@@ -1,3 +1,31 @@
 <?php
 
+class TGenero{
+    private $genero;
+
+    function __construct($genero){
+        $this->genero = $genero;
+    }
+
+    /**
+     * Get the value of genero
+     */ 
+    public function getGenero()
+    {
+        return $this->genero;
+    }
+
+    /**
+     * Set the value of genero
+     *
+     * @return  self
+     */ 
+    public function setGenero($genero)
+    {
+        $this->genero = $genero;
+
+        return $this;
+    }
+}
+
 ?>
