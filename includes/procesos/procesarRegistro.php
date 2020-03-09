c25970b4b86cbfa31b12bd78708211958c3161f3
Dani <53879220+GeoDNSO@users.noreply.github.com>
2020-03-09 21:08:46 +0100

    Funcionamiento básico del procesamiento de registro

--- includes/procesos/procesarRegistro.php
+++ includes/procesos/procesarRegistro.php
@@ -1,27 +1,55 @@
 <?php
     
-include_once("includes/appBooxChange.php");
-include_once("includes/constants.php");
+include_once("../appBooxChange.php");
+include_once("../constants.php");
 
 $app = appBooxChange::getInstance();
 
+$_SESSION[REG_PASS_EQ] = true;
+$_SESSION[REG_DATA_NO_SET] = false;
 
 //Id se puede dejar nulo
 
-$nombreUsuario = $_POST[REG_USERNAME];
-$nombreReal = $_POST[REG_REALNAME];
-$correo = $_POST[REG_EMAIL];
-$password = $_POST[REG_PASS];
-$passwordR = $_POST[REG_PASSR];
-$fotoPerfil = $_POST[REG_FOTO]; 
-$fechaNacimiento= $_POST[REG_FECHA_NAC];
+//Obtener y limpiar los datos 
+$nombreUsuario = htmlspecialchars($_POST[REG_USERNAME]);
+$nombreReal = htmlspecialchars($_POST[REG_REALNAME]);
+$correo = htmlspecialchars($_POST[REG_EMAIL]);
+$password = htmlspecialchars($_POST[REG_PASS]);
+$passwordR = htmlspecialchars($_POST[REG_PASSR]);
+$fotoPerfil = htmlspecialchars($_POST[REG_FOTO]); 
+$fechaNacimiento = htmlspecialchars($_POST[REG_FECHA_NAC]);
 $rol = BD_TYPE_NORMAL_USER; //Usuario normal por defecto
-$ciudad= $_POST[REG_CIUDAD];
-$direccion= $_POST[REG_DIRECCION];
+$ciudad = htmlspecialchars($_POST[REG_CIUDAD]);
+$direccion= htmlspecialchars($_POST[REG_DIRECCION]) ;
+
+
+//Comprobar con la funcion empty
+//Comprobar si se han introducido los datos
+if(!isset($nombreUsuario) || !isset($nombreReal) || !isset($correo) || 
+    !isset($password) || !isset($passwordR) || !isset($fechaNacimiento) || !isset($ciudad) || !isset($direccion)){
+        echo "algun campo vacio o nulo";
+        $_SESSION[REG_DATA_NO_SET] = true;
+}
+
+
+if($passwordR != $password){
+    echo "Contraseñas diferentes";
+    $_SESSION[REG_PASS_EQ] = false;
+}
+
 
 date_default_timezone_set("Europe/Madrid");
 $fechaDeCreacion = date_default_timezone_get(); //REVIsAR FORMATO PARA LA BASE DE DATOS??
 
-$app->registrarUsuario($idUsuario, $nombreUsuario, $nombreReal, $correo, $password, $fotoPerfil, $fechaNacimiento, $rol, $ciudad, $direccion, $fechaDeCreacion)
+if($_SESSION[REG_DATA_NO_SET] == false && $_SESSION[REG_PASS_EQ] == true){
+    echo "Registro bien";
+    $password = password_hash($password, PASSWORD_BCRYPT);
+    //El id se ignorará, se asignara automáticamente
+    $app->registrarUsuario("", $nombreUsuario, $nombreReal, $correo, $password, $fotoPerfil, $fechaNacimiento, $rol, 
+                                $ciudad, $direccion, $fechaDeCreacion);
+
+}
+
+
 
 ?>
