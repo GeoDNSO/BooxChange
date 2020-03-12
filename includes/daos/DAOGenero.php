fb7d881da2500f9304d8e5f62204011e174c933e
Jpz777 <49988587+Jpz777@users.noreply.github.com>
2020-03-12 12:41:24 +0100

    Update DAOGenero.php

--- includes/daos/DAOGenero.php
+++ includes/daos/DAOGenero.php
@@ -1,4 +1,38 @@
 <?php
+include_once("../transfers/TGenero.php");
+include_once("DAO.php");
 
+class DAOGenero extends DAO{
+
+    private static $instance;
+
+    function __construct(){
+        parent::__construct();
+    }
+    
+    public static function getInstance()
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new DAOGenero();
+        }
+        return self::$instance;
+    }
+    
+    //Buscar generos
+
+    public function getAllGerns(){
+        
+    }
+
+    function buscarGenerosporNombreGenero($idGenero){
+
+    }
+
+    //Actualizar Datos
+    function eliminarGenero($idGenero){
+
+    }
+
+}
 
 ?>
