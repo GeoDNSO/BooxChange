bb6f9722dfba284a5a1d70a5822b3d107f727bd7
Jpz777 <49988587+Jpz777@users.noreply.github.com>
2020-03-17 20:57:43 +0100

    Update modificarUsuario.php

--- modificarUsuario.php
+++ modificarUsuario.php
@@ -8,9 +8,22 @@
 
 
     <?php
-        include("includes/comun/cabecera.php")
+        include("includes/comun/cabecera.php");
+
     ?>
 
+    <body>
+        <!--falta la direccion y que luego cambie en la base de datos-->
+        <form action="accion.php" method="post">
+        <p> Nombre Real: <input type="text" name="nombreReal" value="<?php echo $_SESSION['nombreReal']; ?>"/></p>
+        <p> Correo: <input type="text" name="correo" value="<?php echo $_SESSION['correo']; ?>"/></p>
+        <p> Foto: <input type="text" name="foto" value="<?php echo $_SESSION['fotoPerfil']; ?>"/></p>
+        <p> Ciudad: <input type="text" name="ciudad" value="<?php echo $_SESSION['ciudad']; ?>"/></p>
+        <p> Direccion: <input type="text" name="direccion" value="<?php echo $_SESSION['direccion']; ?>"/></p>
 
+        <p><input type="submit" name="accept" value="Cambiar" /></p>
+        <p><input type="button" name="cancel" value="Cancelar" onClick="window.location.href='usuario.php';" /></p>
+        </form>
+    </body>
 
 </html>
