8589b48fdf233be3f2972c598f93cea49dd9921a
Jpz777 <49988587+Jpz777@users.noreply.github.com>
2020-03-25 20:48:05 +0100

    Arrglando cosas

--- includes/constants.php
+++ includes/constants.php
@@ -73,6 +73,7 @@ define("NO_ES_OFERTA", 0);
 
 define("INTERCAMBIADO", 1);
 define("NO_INTERCAMBIADO", 0);
+define("LIBRO_RECHAZADO", 2);
 
 //Claves para la tabla de Ofertas de Intercambios de la Base de Datos
 define("BD_OFERTA_INTER_ID", "id");
@@ -82,6 +83,7 @@ define("BD_OFERTA_INTER_OFERTA_ACEPTADA", "ofertaAceptada");
 
 define("OFERTA_ACEPTADA", 1);
 define("OFERTA_RECHAZADA", 0);
+define("OFERTA_DISPONIBLE", 2);
 
 
 //Claves para la tabla de Intercambios de la Base de Datos
@@ -98,9 +100,16 @@ define("ERROR", -1);
 define("INTERCAMBIO_NO_ENCONTRADO", 0);
 define("INTERCAMBIO_ENCONTRADO", 1);
 
-
+//Claves para la tabla de Genero de la Base de Datos
 define("BD_GENERO_GENERO","Genero");
 
+
+//Claves para la tabla de Notificaciones de la Base de Datos
+define("BD_NOTIFICACION_ID", "id");
+define("BD_NOTIFICACION_ID_USUARIO", "idUsuario");
+define("BD_NOTIFICACION_MENSAJE", "mensaje");
+define("BD_NOTIFICACION_LEIDO", "leido");
+define("BD_NOTIFICACION_FECHA", "fecha");
 
 //Clavos para la tabla de ValoracionLibro de la Base de Datos
 define("BD_VALORACIONLIBRO_ID", "id_valoracion");
@@ -108,6 +117,7 @@ define("BD_VALORACIONLIBRO_IDLIBRO", "id_libro");
 define("BD_VALORACIONLIBRO_IDUSUARIO", "id_usuario");
 define("BD_VALORACIONLIBRO_VALORACION", "valoracion");
 define("BD_VALORACIONLIBRO_COMENTARIO", "comentario");
+
 //Constantes para la tabla de Temas
 define("BD_TEMA", "Tema");
 //Constantes para la tabla de Discusiones
--- includes/daos/DAOOfertasIntercambio.php
+++ includes/daos/DAOOfertasIntercambio.php
@@ -5,7 +5,7 @@ $parentDir = dirname(__DIR__, 1);
 require_once($parentDir."/config.php");
 
 use fdi\ucm\aw\booxchange\transfers\TOfertasIntercambio as TOfertasIntercambio;
-
+use fdi\ucm\aw\booxchange\transfers\TLibroIntercambio;
 
 
 class DAOOfertasIntercambio extends DAO
@@ -25,8 +25,78 @@ class DAOOfertasIntercambio extends DAO
         return self::$instance;
     }
 
+    //INSERT INTO `ofertasintercambio` (`id`, `idLibroIntercambio`, `idLibroOferta`, `ofertaAceptada`) VALUES (NULL, '3', '48', NULL);
     
+    public function subirOferta($libroQuerido, $libroOfertado)
+    {
+        $idLibroQuerido = $libroQuerido->getIdLibroInter();
+        $idLibroOfertado = $libroOfertado->getIdLibroInter();
+        $estado = OFERTA_DISPONIBLE;
+
+        $sql = "INSERT INTO `ofertasintercambio` (`id`, `idLibroIntercambio`, `idLibroOferta`, `ofertaAceptada`)
+                 VALUES (NULL, '$idLibroQuerido', '$idLibroOfertado', $estado);";
+        
+        return mysqli_query(self::$instance->bdBooxChange, $sql);
+    }
+
+    public function actualizarOferta($ofertaAceptada, $idOferta){
+
+        $opt = ($ofertaAceptada == true) ? "1" : "0";
+        $sql = "UPDATE `ofertasintercambio` SET `ofertaAceptada` = '$opt' WHERE `ofertasintercambio`.`id` = $idOferta;";
+        
+        return mysqli_query(self::$instance->bdBooxChange, $sql);
+    }
+
+
+    //SELECT DISTINCT id, idLibroIntercambio FROM ofertasintercambio JOIN librointercambio ON ofertasintercambio.idLibroIntercambio=librointercambio.Id_Libro_Inter WHERE librointercambio.Id_Usuario=6
+    public function getOfertas($idUsuario)
+    {
+        $sql = "SELECT DISTINCT id, idLibroIntercambio FROM ofertasintercambio 
+                JOIN librointercambio ON ofertasintercambio.idLibroIntercambio=librointercambio.Id_Libro_Inter 
+                WHERE librointercambio.Id_Usuario=$idUsuario AND librointercambio.Intercambiado!=1;";
+        
+        $consulta = mysqli_query(self::$instance->bdBooxChange, $sql);
+        $array = array();
+        while( $fila = $consulta->fetch_array()){
+            $tOferta = new TOfertasIntercambio($fila[BD_OFERTA_INTER_ID], $fila[BD_OFERTA_INTER_ID_LIBRO_INTER], null, null);
+            $array[] = $tOferta;
+        }
+                
+        return $array;
+    }
+
+    public function getOfertasLibro($id)
+    {
+        $vDisponible = OFERTA_DISPONIBLE;
+        $sql = "SELECT * FROM ofertasintercambio 
+                JOIN librointercambio ON ofertasintercambio.idLibroIntercambio=librointercambio.Id_Libro_Inter 
+                WHERE librointercambio.Id_Libro_Inter=$id AND ofertasintercambio.ofertaAceptada=$vDisponible;";
+        
+        $consulta = mysqli_query(self::$instance->bdBooxChange, $sql);
+        $array = array();
+        while( $fila = $consulta->fetch_array()){
+            $tOferta = new TOfertasIntercambio($fila[BD_OFERTA_INTER_ID], $fila[BD_OFERTA_INTER_ID_LIBRO_INTER], $fila[BD_OFERTA_INTER_ID_LIBRO_OFERTA], $fila[BD_OFERTA_INTER_OFERTA_ACEPTADA]);
+            $array[] = $tOferta;
+        }
+                
+        return $array;
+
+    }
+
+    public function getNumOfertas($idLibro)
+    {
+        $sql = "SELECT COUNT(idLibroIntercambio) FROM ofertasintercambio WHERE ofertaAceptada = 2 AND ofertasintercambio.idLibroIntercambio=$idLibro";
+        $consulta = mysqli_query(self::$instance->bdBooxChange, $sql);
+        if (mysqli_num_rows($consulta) == 1) {
+            $fila = $consulta->fetch_array();
+            return $fila[0];
+        } else {
+            return null;
+        }
+    }
 
 }
+
+
 
 ?>
