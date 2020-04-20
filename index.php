436c34d7808ec7561f9202e399592e480c52c691
Dani <dnsodani@gmail.com>
2020-04-20 22:49:35 +0200

    css index incompleto

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    require_once(__DIR__."/includes/config.php");
+require_once(__DIR__ . "/includes/config.php");
 ?>
 
 <!DOCTYPE html>
@@ -17,21 +17,58 @@
 include("includes/comun/cabecera.php");
 ?>
 
-<h1>De que trata Booxchange</h1>
+
 
 <body>
-    
-<div class="main">
 
-<p>BooxChange es una web centrada en el intercambio y compraventa de libros, para así promover y divertirse con lectura ya sea intercambiando títulos con otras personas en las mismas condiciones, debatiendo en el foro o descubriendo nuevos libros.</p>
+    <div class="main">
 
-</div>
+        
 
-    
+        <div class="mejoresLibros">
+
+            <div class="libroPresentacion1">
+
+                <div class="imgPresentacion">
+                    <img src="./imagenes/libros/default.jpg" alt="adas">
+                </div>
+
+                <div class="descLibroPresentacion">
+                    <h2>Libro 1 de Autor Apellidos </h2>
+                    
+                </div>
+            </div>
+
+            <div class="libroPresentacion2">
+
+                <div class="descLibroPresentacion">
+                    <h2>Libro 2 de Autor Apellidos</h2>
+                </div>
+
+                <div class="imgPresentacion">
+                    <img src="./imagenes/libros/default.jpg" alt="adas">
+                </div>
+
+
+            </div>
+
+        </div>
+
+        <div class="presentacion">
+            <h1>De que trata Booxchange</h1>
+            <p>BooxChange es una web centrada en el intercambio y compraventa de libros, para así promover y divertirse con lectura ya sea intercambiando títulos con otras personas en las mismas condiciones, debatiendo en el foro o descubriendo nuevos libros.</p>
+        </div>
+
+
 
 </body>
 
 
+<footer>
+    <?php
+    include("./includes/comun/footer.php")
+    ?>
+</footer>
 
 
 
