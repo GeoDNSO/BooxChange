3b54a6efb0a371b601db5d1a8dbcce2909f2cdd7
Alfon <altercer@ucm.es>
2020-05-06 12:34:00 +0200

    Revert "Merge branch 'Development' of https://github.com/GeoDNSO/BooxChange into Development"
    This reverts commit 0bde41792250813452b989ffe1ec5efae560fa12, reversing
    changes made to 36ee58f5056d955b7ecaa4343403900ce7e99a03.

--- includes/comun/cabecera.php
+++ includes/comun/cabecera.php
@@ -29,15 +29,14 @@ function perfilUsuario()
         $app = appBooxChange::getInstance();
         $numNotificaciones = $app->notificacionesUsuario($_SESSION["id_Usuario"]);
         $notificacionesCab = ($numNotificaciones == 0) ? "" : " ($numNotificaciones)";
-        echo "<span class='userNameHeader'>".$_SESSION['nombreReal'] ."</span>";
+        echo "<span class='userNameHeader'>".$_SESSION['nombreReal']."</span>";
         echo '<svg class="arrowUser" width="8" height="5" viewBox="0 0 8 5" class="arrow-down" fill="#0BC4E2" xmlns="http://www.w3.org/2000/svg"><path d="M0.707109 1.70711L3.29289 4.29289C3.68342 4.68342 4.31658 4.68342 4.70711 4.29289L7.29289 1.70711C7.92286 1.07714 7.47669 0 6.58579 0H1.41421C0.523309 0 0.0771438 1.07714 0.707109 1.70711Z"></path></svg>';
 
         echo "<ul>";
 
         echo "<li> <a href='usuario.php'>" . "Perfil" . "</a> </li>";
         echo "<li> <a href='ofertas.php'> Tus Ofertas</a> </li>";
-        //"<span class='headerNotification'> <p>2<p> </span>"
-        echo "<li class='notHeader'><a href='notificaciones.php'>Notificaciones$notificacionesCab" ."</a></li>";
+        echo "<li><a href='notificaciones.php'>Notificaciones$notificacionesCab</a></li>";
         echo "<li> <a href='chat.php'> Chatear </a> </li>";
 
         if ($_SESSION['rol'] == BD_TYPE_ADMIN) {
