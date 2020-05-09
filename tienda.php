c6812bf09fa071ebdb1af4a648cbd746d6172676
MondlichtFox <svillanuevacanada@gmail.com>
2020-05-09 10:57:37 +0200

    Tienda semiarreglado

--- tienda.php
+++ tienda.php
@@ -27,11 +27,11 @@ $librosTienda = $app->librosTienda();
 
 $html = '<div id="buscaLibro" class = "buscarTienda">';
 $html .= '<form method="post">';
-+$html .= '<div class="fields noWrapBuscar">';
-+//$html .= '    <label for="titulo"><b>Buscar Libro por título:</b><br></label><br>';
-+$html .= '     <div class="text ancho"> <input class="login" placeholder="   Introduzca título del libro..." type=text name="titulo" id="titulo" /></div>';
-+
-+$html .= '    <div class= selectCentrado><select id="genero" class=marginGenero name="genero">';
+$html .= '<div class="fields noWrapBuscar">';
+//$html .= '    <label for="titulo"><b>Buscar Libro por título:</b><br></label><br>';
+$html .= '     <div class="text ancho"> <input class="login" placeholder="   Introduzca título del libro..." type=text name="titulo" id="titulo" /></div>';
+
+$html .= '    <div class= selectCentrado><select id="genero" class=marginGenero name="genero">';
 $html .=      $app->construirSeleccionDeCategorias();
 $html .= '    </select></div>';
 $html .= '    <button class="send-button noEnorme marginTop">Buscar</button>';
