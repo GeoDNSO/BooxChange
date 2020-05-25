334fb90ae452a959390b25dc2bba4ee605f07862
Dani <dnsodani@gmail.com>
2020-05-25 16:39:40 +0200

    Modo Oscuro fixed

--- AD_cambiarRol.php
+++ AD_cambiarRol.php
@@ -1,81 +1,87 @@
 <?php
-    require_once(__DIR__ . "/includes/config.php");
-?><!DOCTYPE html>
+require_once(__DIR__ . "/includes/config.php");
+?>
+<!DOCTYPE html>
 <html lang="es">
 
 <head>
     <title>BooxChange</title>
     <meta charset="UTF-8" />
     <link rel="icon" href="./favicon.ico" type="image/x-icon" />
-    
+
     <link rel="stylesheet" type="text/css" href="css/estilo.css" />
-    <link rel="stylesheet" id="estiloRoot" type="text/css" href="css/root.css" />
-    
+
+    <?php
+    if (!isset($_COOKIE["estiloWeb"]) || $_COOKIE["estiloWeb"] == "claro") {
+        echo '<link rel="stylesheet" id="estiloRoot" type="text/css" href="css/root.css" />';
+    } else {
+        echo '<link rel="stylesheet" id="estiloRoot" type="text/css" href="css/root_dark_mode.css" />';
+    }
+    ?>
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script type="text/javascript" src="javascript/config.js"></script>
 </head>
 
 <?php
-    include("includes/comun/cabecera.php");
+include("includes/comun/cabecera.php");
 
-    include("./includes/comun/funcionesAdmin.php");
+include("./includes/comun/funcionesAdmin.php");
 
-    use \fdi\ucm\aw\booxchange\appBooxChange as appBooxChange;
+use \fdi\ucm\aw\booxchange\appBooxChange as appBooxChange;
 
-    if (!isset($_GET['id'])) {
-        exit("No se ha proporcionado el id del user");
-    } else if (!isset($_SESSION['login']) || $_SESSION['login'] == false) {
-        exit("Usuario no logeado, no se puede modificar el rol");
-    } else if ($_SESSION['rol'] != BD_TYPE_ADMIN){
-        exit("No tienes permisos de admin");
-    } else {
-        $app = appBooxChange::getInstance();
-        $user = $app->getUserById($_GET['id']);
-        $_SESSION["modificarRol"] = serialize($_GET['id']);
+if (!isset($_GET['id'])) {
+    exit("No se ha proporcionado el id del user");
+} else if (!isset($_SESSION['login']) || $_SESSION['login'] == false) {
+    exit("Usuario no logeado, no se puede modificar el rol");
+} else if ($_SESSION['rol'] != BD_TYPE_ADMIN) {
+    exit("No tienes permisos de admin");
+} else {
+    $app = appBooxChange::getInstance();
+    $user = $app->getUserById($_GET['id']);
+    $_SESSION["modificarRol"] = serialize($_GET['id']);
 
-        $nombreReal = $user -> getNombreReal();
-        $rol = $user -> getRol();
-    }
+    $nombreReal = $user->getNombreReal();
+    $rol = $user->getRol();
+}
 
-    echo "<div class='modifPerfil'>";
-    echo "<h3 class=nombreReal>Nombre Real: $nombreReal</h3>";
+echo "<div class='modifPerfil'>";
+echo "<h3 class=nombreReal>Nombre Real: $nombreReal</h3>";
 ?><form method="post" action="includes/procesos/AD_procesarCambiarRol.php" enctype="multipart/form-data">
 
-        <label for="rol"><b>Seleccionar rol</b></label><br>
-        <?php
+    <label for="rol"><b>Seleccionar rol</b></label><br>
+    <?php
 
-        if($rol == 0){
-            echo '<input type="radio" class=marginTopRol id="0" name="rol" value="0" checked>';
-            echo '&nbsp<label for="0">Administrador</label><br>';
-            echo '<input type="radio" class=marginTopRol id="1" name="rol" value="1">';
-            echo '&nbsp<label for="1">Usuario registrado</label><br>';
-            echo '<input type="radio" class=marginTopRol id="2" name="rol" value="2">';
-            echo '&nbsp<label for="2">Moderador</label><br><br>';
-        }
+    if ($rol == 0) {
+        echo '<input type="radio" class=marginTopRol id="0" name="rol" value="0" checked>';
+        echo '&nbsp<label for="0">Administrador</label><br>';
+        echo '<input type="radio" class=marginTopRol id="1" name="rol" value="1">';
+        echo '&nbsp<label for="1">Usuario registrado</label><br>';
+        echo '<input type="radio" class=marginTopRol id="2" name="rol" value="2">';
+        echo '&nbsp<label for="2">Moderador</label><br><br>';
+    } else if ($rol == 2) {
+        echo '<input type="radio" class=marginTopRol id="0" name="rol" value="0">';
+        echo '&nbsp<label for="0">Administrador</label><br>';
+        echo '<input type="radio" class=marginTopRol id="1" name="rol" value="1">';
+        echo '&nbsp<label for="1">Usuario registrado</label><br>';
+        echo '<input type="radio" class=marginTopRol id="2" name="rol" value="2" checked>';
+        echo '&nbsp<label for="2">Moderador</label><br><br>';
+    } else {
+        echo '<input type="radio" class=marginTopRol id="0" name="rol" value="0">';
+        echo '&nbsp<label for="0">Administrador</label><br>';
+        echo '<input type="radio" class=marginTopRol id="1" name="rol" value="1" checked>';
+        echo '&nbsp<label for="1">Usuario registrado</label><br>';
+        echo '<input type="radio" class=marginTopRol id="2" name="rol" value="2">';
+        echo '&nbsp<label for="2">Moderador</label><br><br>';
+    }
+    ?><input type="submit" class="send-button noEnorme">
 
-        else if($rol == 2){
-            echo '<input type="radio" class=marginTopRol id="0" name="rol" value="0">';
-            echo '&nbsp<label for="0">Administrador</label><br>';
-            echo '<input type="radio" class=marginTopRol id="1" name="rol" value="1">';
-            echo '&nbsp<label for="1">Usuario registrado</label><br>';
-            echo '<input type="radio" class=marginTopRol id="2" name="rol" value="2" checked>';
-            echo '&nbsp<label for="2">Moderador</label><br><br>';
-        }
-
-        else{
-            echo '<input type="radio" class=marginTopRol id="0" name="rol" value="0">';
-            echo '&nbsp<label for="0">Administrador</label><br>';
-            echo '<input type="radio" class=marginTopRol id="1" name="rol" value="1" checked>';
-            echo '&nbsp<label for="1">Usuario registrado</label><br>';
-            echo '<input type="radio" class=marginTopRol id="2" name="rol" value="2">';
-            echo '&nbsp<label for="2">Moderador</label><br><br>';
-        }
-        ?><input type="submit" class="send-button noEnorme">
-
-    </form>
-    <a class="send-button cancelar" href="AD_listaUsuarios.php"> Cancelar </a>
-    </div>
+</form>
+<a class="send-button cancelar" href="AD_listaUsuarios.php"> Cancelar </a>
+</div>
 
 <?php
 include("./includes/comun/footer.php");
-?></html>
+?>
+
+</html>
--- libroTienda.php
+++ libroTienda.php
@@ -1,142 +1,150 @@
 <?php
-    require_once(__DIR__."/includes/config.php");
-?><!DOCTYPE html>
+require_once(__DIR__ . "/includes/config.php");
+?>
+<!DOCTYPE html>
 <html lang="es">
-    <head>
-        <title>BooxChange Tienda</title>
-        <meta charset="UTF-8" />
-        <link rel="icon" href="./favicon.ico" type="image/x-icon" />
-        
-        <link rel="stylesheet" type="text/css" href="css/estilo.css" />
-    <link rel="stylesheet" id="estiloRoot" type="text/css" href="css/root.css" />
-    
+
+<head>
+    <title>BooxChange Tienda</title>
+    <meta charset="UTF-8" />
+    <link rel="icon" href="./favicon.ico" type="image/x-icon" />
+
+    <link rel="stylesheet" type="text/css" href="css/estilo.css" />
+    <?php
+    if (!isset($_COOKIE["estiloWeb"]) || $_COOKIE["estiloWeb"] == "claro") {
+        echo '<link rel="stylesheet" id="estiloRoot" type="text/css" href="css/root.css" />';
+    } else {
+        echo '<link rel="stylesheet" id="estiloRoot" type="text/css" href="css/root_dark_mode.css" />';
+    }
+    ?>
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script type="text/javascript" src="javascript/config.js"></script>
-    </head>
+</head>
 
 <?php
 
-    include_once(__DIR__."/includes/comun/cabecera.php");
-    //include_once(__DIR__."./includes/constants.php");
-    //include_once(__DIR__."/includes/appBooxChange.php");
-    //include_once(__DIR__."/includes/transfers/TLibro.php");
+include_once(__DIR__ . "/includes/comun/cabecera.php");
+//include_once(__DIR__."./includes/constants.php");
+//include_once(__DIR__."/includes/appBooxChange.php");
+//include_once(__DIR__."/includes/transfers/TLibro.php");
 
 
 
-    use \fdi\ucm\aw\booxchange\appBooxChange as appBooxChange;
-    use fdi\ucm\aw\booxchange\formularios\FormularioValorar;
-    echo "<body>";
-    if(!isset($_GET['id'])){
-        exit("No se ha proporcionado el id del producto");
+use \fdi\ucm\aw\booxchange\appBooxChange as appBooxChange;
+use fdi\ucm\aw\booxchange\formularios\FormularioValorar;
+
+echo "<body>";
+if (!isset($_GET['id'])) {
+    exit("No se ha proporcionado el id del producto");
+} else {
+    echo "<div class=contenidoLibroTienda>";
+    echo "<div id='libro'>";
+    $id = $_GET['id'];
+    $app = appBooxChange::getInstance();
+    $libro = $app->getLibroById($id);
+
+    echo "<div id='portadaCompra'>";
+    $imagen = $libro->getImagen();
+    echo "<img src='$imagen' alt='Imagen del Libro' height='450' width='300'>  <br>";
+
+    $precio = $libro->getPrecio();
+    echo "<h3 class=marginTopPrecio> Precio: <span>$precio €</span></h3>";
+    $unidades = $libro->getUnidades();
+    echo "<h3 id='lastH3'> Unidades: <span>$unidades disponibles</span></h3>";
+    if ($unidades > 0) {
+        echo "<p class=botonCentrado><a class='libroTiendaBoton' href='paginaCompra.php?id=$id'> Comprar </a></p>";
+    } else {
+        echo "Existencias Agotadass";
     }
-    else{
-    	echo "<div class=contenidoLibroTienda>";
-        echo "<div id='libro'>";
-        $id = $_GET['id'];
-        $app = appBooxChange::getInstance();
-        $libro = $app->getLibroById($id);
+    echo "</div>";
+    echo "<div id='datos'>";
+    $titulo = $libro->getTitulo();
+    echo "<h2 id='titulo'>$titulo </h2>";
 
-        echo "<div id='portadaCompra'>";
-        $imagen = $libro->getImagen();
-        echo "<img src='$imagen' alt='Imagen del Libro' height='450' width='300'>  <br>";
+    $autor = $libro->getAutor();
+    echo "<h3>Autor/a:<span> $autor </span></h3>";
 
-        $precio = $libro->getPrecio();
-        echo "<h3 class=marginTopPrecio> Precio: <span>$precio €</span></h3>";
-        $unidades = $libro->getUnidades();
-        echo "<h3 id='lastH3'> Unidades: <span>$unidades disponibles</span></h3>";
-        if($unidades > 0){
-            echo "<p class=botonCentrado><a class='libroTiendaBoton' href='paginaCompra.php?id=$id'> Comprar </a></p>";
-        }
-        else{
-            echo "Existencias Agotadass";
-        }
-        echo "</div>";
-        echo "<div id='datos'>";
-        $titulo = $libro->getTitulo();
-        echo "<h2 id='titulo'>$titulo </h2>";
-
-        $autor = $libro->getAutor();
-        echo "<h3>Autor/a:<span> $autor </span></h3>";
-
-        $valoracion = $libro->getValoracion();
-        $valoracion = round($valoracion, 2);
-        $valoracionEstrellas = round($valoracion);
-        echo "<h3> Valoración: ";
-        for ($i = 0; $i < $valoracionEstrellas; $i++){
-            echo "<span class='valoracion coloreada'>★</span>";
-        }
-        while ($i < 5){
-            echo "<span class='valoracion'>★</span>";
-            $i++;
-        }
-        echo "<span> $valoracion de 5</span>";
+    $valoracion = $libro->getValoracion();
+    $valoracion = round($valoracion, 2);
+    $valoracionEstrellas = round($valoracion);
+    echo "<h3> Valoración: ";
+    for ($i = 0; $i < $valoracionEstrellas; $i++) {
+        echo "<span class='valoracion coloreada'>★</span>";
+    }
+    while ($i < 5) {
+        echo "<span class='valoracion'>★</span>";
+        $i++;
+    }
+    echo "<span> $valoracion de 5</span>";
 
 
-        echo "</h3>";
+    echo "</h3>";
 
-        /*
+    /*
         $ranking = $libro->getRanking();
         echo "<h3> Ranking: $ranking </h3>";
 */
 
-        $genero = $libro->getGenero();
-        echo "<h3> Género: <span>$genero </span></h3>";
+    $genero = $libro->getGenero();
+    echo "<h3> Género: <span>$genero </span></h3>";
 
-        $fechaPublicacion = $libro->getFechaPublicacion();
-        echo "<h3> Fecha de publicación: <span>$fechaPublicacion </span></h3>";
+    $fechaPublicacion = $libro->getFechaPublicacion();
+    echo "<h3> Fecha de publicación: <span>$fechaPublicacion </span></h3>";
 
-        $idioma = $libro->getIdioma();
-        echo "<h3> Idioma: <span>$idioma </span></h3>";
+    $idioma = $libro->getIdioma();
+    echo "<h3> Idioma: <span>$idioma </span></h3>";
 
-        $editorial = $libro->getEditorial();
-        echo "<h3> Editorial: <span>$editorial </span></h3>";
+    $editorial = $libro->getEditorial();
+    echo "<h3> Editorial: <span>$editorial </span></h3>";
 
-        $descripcion = $libro->getDescripcion();
-        echo "<h3> Descripción: <span>$descripcion</span> </h3>";
+    $descripcion = $libro->getDescripcion();
+    echo "<h3> Descripción: <span>$descripcion</span> </h3>";
 
-        echo "</div></div>";
+    echo "</div></div>";
 
-        echo "<div id='valoraciones'>";
-        echo "<div id='comentarios'>";
+    echo "<div id='valoraciones'>";
+    echo "<div id='comentarios'>";
 
-        $valoracionesLibro = $app->valoracionesLibro($id);
-        $numValoraciones = count($valoracionesLibro);
+    $valoracionesLibro = $app->valoracionesLibro($id);
+    $numValoraciones = count($valoracionesLibro);
 
-        echo "<h1>Comentarios [$numValoraciones]</h1><ul>";
+    echo "<h1>Comentarios [$numValoraciones]</h1><ul>";
 
-        foreach ($valoracionesLibro as $valoracionLibro) {
-            $puntuacion = $valoracionLibro->getValoracion();
-            $comentario = $valoracionLibro->getComentario();
-            $idUsuario = $valoracionLibro->getIdUsuario();
-            $usuario = $app->getUserById($idUsuario);
-            $nombreUsuario = $usuario->getNombreUsuario();
+    foreach ($valoracionesLibro as $valoracionLibro) {
+        $puntuacion = $valoracionLibro->getValoracion();
+        $comentario = $valoracionLibro->getComentario();
+        $idUsuario = $valoracionLibro->getIdUsuario();
+        $usuario = $app->getUserById($idUsuario);
+        $nombreUsuario = $usuario->getNombreUsuario();
 
-            echo "<li><div class='todaValoracion'>";
-            echo "<h2 class='nombreUsuario'>" . $nombreUsuario . "</h2>";
+        echo "<li><div class='todaValoracion'>";
+        echo "<h2 class='nombreUsuario'>" . $nombreUsuario . "</h2>";
 
-            $i=0;
-            for ($i = 0; $i < $puntuacion; $i++){
-                echo "<span class='valoracion coloreada'>★</span>";
-            }
-            while ($i < 5){
-                echo "<span class='valoracion'>★</span>";
-                $i++;
-            }
-            if ($comentario != null){
-                echo "<p class='comentarioValoracion'>" . $comentario . "<br>";
-            }
-            echo "</div></li>";
+        $i = 0;
+        for ($i = 0; $i < $puntuacion; $i++) {
+            echo "<span class='valoracion coloreada'>★</span>";
         }
-        echo "</ul></div>";
-
-        $form = new FormularioValorar("valorarForm", array("action" =>"./includes/procesos/procesarValorar.php", "libroId" => $id));
-        $form->gestiona();
-        echo "</div>";
-        echo "</div>";
-
+        while ($i < 5) {
+            echo "<span class='valoracion'>★</span>";
+            $i++;
+        }
+        if ($comentario != null) {
+            echo "<p class='comentarioValoracion'>" . $comentario . "<br>";
+        }
+        echo "</div></li>";
     }
+    echo "</ul></div>";
 
-    echo "</body>";
-    include("./includes/comun/footer.php");
+    $form = new FormularioValorar("valorarForm", array("action" => "./includes/procesos/procesarValorar.php", "libroId" => $id));
+    $form->gestiona();
+    echo "</div>";
+    echo "</div>";
+}
 
-?></html>
+echo "</body>";
+include("./includes/comun/footer.php");
+
+?>
+
+</html>
